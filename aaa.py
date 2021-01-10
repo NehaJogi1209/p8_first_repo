a6b18df7f94581798e06053fa99633e07cf6b222
NehaJogi1209 <nehajogi12@gmail.com>
2021-01-10 18:25:24 +0530

    my second commit

--- aaa.py
+++ aaa.py
@@ -1,3 +1,6 @@
 print("Welcome to the first git demo..!!")
 name = input("Eniter your name :")
-print("Good Eening,{}...!!!".format(name))
+print("Good Evening,{}...!!!".format(name))
+
+
+print("My first commit done..!!")
